b3e0b7e087246bf59311f063352ea36d07b5450d
Ricco555 <rikolusa@gmail.com>
2026-05-05 20:56:53 +0200

    move v1 netflow code

--- netflow/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from models.edge_head import EdgeHead
-from models.edge_graphsage import EdgeGraphSAGE
-from models.gcn import GCNEdgeClassifier
-from models.gat import GATEdgeClassifier
-
-__all__ = ["EdgeHead", "EdgeGraphSAGE", "GCNEdgeClassifier", "GATEdgeClassifier"]
